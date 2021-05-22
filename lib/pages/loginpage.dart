24676ed09ba5722ef339ac2d65fa8a611660d170
KV <kvdigitalise@gmail.com>
2021-05-22 17:29:48 +0530

    day6 Completed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool clickButton = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,12 +19,15 @@ class LoginPage extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.asset("assets/images/login_image.png",fit: BoxFit.contain,),
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.contain,
+              ),
               SizedBox(
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
@@ -34,6 +44,11 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter Username", labelText: "Username"),
+                      onChanged: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
                     ),
                     SizedBox(
                       height: 20.0,
@@ -46,18 +61,49 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // print("Vedant");
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          clickButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      style: TextButton.styleFrom(minimumSize: Size(150, 45)),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: clickButton ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius:
+                                BorderRadius.circular(clickButton ? 50 : 8.0)),
+                        child: clickButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
                       ),
                     )
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     // print("Vedant");
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 45)),
+                    //   child: Text(
+                    //     "Login",
+                    //     style: TextStyle(
+                    //         fontSize: 18.0, fontWeight: FontWeight.bold),
+                    //   ),
+                    // )
                   ],
                 ),
               )
